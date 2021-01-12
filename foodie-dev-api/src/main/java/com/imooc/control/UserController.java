38ba7382aaaa582fb70a342036999460d07ee01e
jiangchao <jiangchao6@iwhalecloud.com>
2021-01-12 16:57:15 +0800

    user表增删改

--- foodie-dev-api/src/main/java/com/imooc/control/UserController.java
+++ foodie-dev-api/src/main/java/com/imooc/control/UserController.java
@@ -4,6 +4,7 @@ import com.imooc.pojo.Users;
 import com.imooc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,11 +16,33 @@ public class UserController {
 
     @GetMapping("/getUser")
     public Object getUser(int userId) {
-//        return userService.getUserById(userId);
         Users users = userService.getUserById(userId);
         if(users != null){
             return users;
         }
         return "查询为空";
+    }
+
+    @PostMapping("/insertUser")
+    public void insertUser(){
+        Users users = new Users();
+        users.setName("jc");
+        users.setAge(18);
+        users.setUserid(3);
+        userService.insertUser(users);
+    }
+
+    @PostMapping("/deleteUser")
+    public void deleteUser(int userid){
+        userService.delUserById(userid);
+    }
+
+    @PostMapping("/updateUser")
+    public void updateUser(){
+        Users users = new Users();
+        users.setName("TomNew");
+        users.setAge(18);
+        users.setUserid(2);
+        userService.updateUser(users);
     }
 }
--- foodie-dev-service/src/main/java/com/imooc/serviceImpl/UserServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/serviceImpl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
 import com.imooc.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -13,21 +15,25 @@ public class UserServiceImpl implements UserService {
     @Resource
     UsersMapper usersMapper;
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void insertUser(Users users) {
-
+        usersMapper.insert(users);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void delUserById(int userId) {
-
+        usersMapper.deleteByPrimaryKey(userId);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void updateUser(Users users) {
-
+        usersMapper.updateByPrimaryKey(users);
     }
 
+    @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public Users getUserById(int userId) {
         return usersMapper.selectByPrimaryKey(userId);
